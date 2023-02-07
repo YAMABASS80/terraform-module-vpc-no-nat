4d06202a2ee5fbd8f1462c8508b9238981da9f5a
Yamabe <myamabe@88665a14de73.ant.amazon.com>
2023-02-07 10:35:16 +0900

    Separates from VPC endpoints from main.tf

--- main.tf
+++ main.tf
@@ -109,28 +109,3 @@ resource "aws_internet_gateway" "igw" {
     Name = "${var.vpc_name}-igw"
   }
 }
-
-resource "aws_vpc_endpoint" "s3" {
-  vpc_id       = aws_vpc.this.id
-  service_name = "com.amazonaws.${data.aws_region.current.name}.s3"
-}
-
-resource "aws_vpc_endpoint_route_table_association" "private_subnet_1" {
-  route_table_id  = aws_route_table.private_subnet_1_route_table.id
-  vpc_endpoint_id = aws_vpc_endpoint.s3.id
-}
-
-resource "aws_vpc_endpoint_route_table_association" "private_subnet_2" {
-  route_table_id  = aws_route_table.private_subnet_2_route_table.id
-  vpc_endpoint_id = aws_vpc_endpoint.s3.id
-}
-
-resource "aws_vpc_endpoint_route_table_association" "public_subnet_1" {
-  route_table_id  = aws_route_table.public_subnet_1_route_table.id
-  vpc_endpoint_id = aws_vpc_endpoint.s3.id
-}
-
-resource "aws_vpc_endpoint_route_table_association" "public_subnet_2" {
-  route_table_id  = aws_route_table.public_subnet_2_route_table.id
-  vpc_endpoint_id = aws_vpc_endpoint.s3.id
-}
